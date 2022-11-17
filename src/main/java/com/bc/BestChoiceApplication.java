d78e87d6311ef1df947e7db876d866a3d6a1652d
Prayag Patel <prayagp8@gmail.com>
2022-11-17 17:49:07 +0530

    blue  print

---
+++ src/main/java/com/bc/BestChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestChoiceApplication.class, args);
+	}
+
+}
