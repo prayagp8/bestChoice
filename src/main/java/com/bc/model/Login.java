3c9176057ecc38d98341cb86112c5a457adeb85b
sanajitjana <dev.sanajitjana@gmail.com>
2022-11-29 17:56:33 +0530

    update login model

--- src/main/java/com/bc/model/Login.java
+++ src/main/java/com/bc/model/Login.java
@@ -1,6 +1,5 @@
 package com.bc.model;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,7 +12,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
-@Entity
 public class Login {
 
 	@Id
