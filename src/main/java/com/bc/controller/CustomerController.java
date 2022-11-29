54486defe375320b1a22ce1cf4345fe79b7231e0
sanajitjana <dev.sanajitjana@gmail.com>
2022-11-29 16:16:33 +0530

    Update customer controller endpoint

--- src/main/java/com/bc/controller/CustomerController.java
+++ src/main/java/com/bc/controller/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bc.exception.CustomerException;
@@ -18,27 +19,28 @@ import com.bc.model.Customer;
 import com.bc.service.CustomerService;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
 
 	@Autowired
 	private CustomerService cService;
 
-	@PostMapping("/customer")
+	@PostMapping("/add")
 	public ResponseEntity<Customer> addCustomer(@RequestBody Customer c) throws CustomerException {
 		return new ResponseEntity<Customer>(cService.addCustomer(c), HttpStatus.OK);
 	}
 
-	@PutMapping("/customer")
+	@PutMapping("/update")
 	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer c) throws CustomerException {
 		return new ResponseEntity<Customer>(cService.updateCustomer(c), HttpStatus.OK);
 	}
 
-	@DeleteMapping("/customer/{cId}")
-	public ResponseEntity<Customer> remove(@PathVariable("cId") Integer cId) throws CustomerException {
-		return new ResponseEntity<Customer>(cService.remove(cId), HttpStatus.OK);
+	@DeleteMapping("/remove/{id}")
+	public ResponseEntity<Customer> remove(@PathVariable("id") Integer customerId) throws CustomerException {
+		return new ResponseEntity<Customer>(cService.remove(customerId), HttpStatus.OK);
 	}
 
-	@GetMapping("/customers")
+	@GetMapping("/view")
 	public ResponseEntity<List<Customer>> viewAllCustomer() throws CustomerException {
 		return new ResponseEntity<List<Customer>>(cService.viewAllCustomer(), HttpStatus.OK);
 	}
