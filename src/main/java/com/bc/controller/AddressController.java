8c13678ab4f059cb088dd5547d6755bf06918f26
sanajitjana <dev.sanajitjana@gmail.com>
2022-11-30 14:29:26 +0530

    remove extra line

--- src/main/java/com/bc/controller/AddressController.java
+++ src/main/java/com/bc/controller/AddressController.java
@@ -2,8 +2,6 @@ package com.bc.controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
